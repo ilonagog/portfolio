1d0208f651e5fd4a4f59c8971d36e00b436a9377
ilona <gogiashvili.ilona@gmail.com>
2023-12-09 19:33:57 -0500

    update

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,17 @@
 
 import React from 'react';
-
 import Projects from './Projects'
 import Blogs from "./Blogs"
 import About from './About'
 
 const Home = () => {
+
   return (
     <>
       <div className='container'>
         <div className='about'>
           <h1 className="title">
-            Hi, I'm Ilona Gogiashvili
+            Hi, I'm Ilona Gogiashvili,
           </h1>
           <p >
             A Full Stack Web Developer
@@ -20,20 +20,16 @@ const Home = () => {
         <div className='subtitle'>
           <h3 >based in NY, NY.</h3>
         </div>
-        {/* <div className='image'>
-          <img className='portrait' src={imageOfMe} alt="portrait" width="250px" height="450px" />
-        </div> */}
+
+        <h2>Skills</h2>
+        <div className='skills'>
+          <h3>
+            JavaScript, React.js, CSS, HTML, Object Oriented Programming, Ruby on Rails, Active Storage, SQL.
+          </h3>
+        </div>
       </div>
-      <div class="spacer"></div>
-      <div class="spacer"></div>
       <div class="spacer"></div>
       <About />
-      <h2>Skills</h2>
-      <div className='skills'>
-        <h3>
-          JavaScript, React.js, CSS, HTML, Object Oriented Programming, Ruby on Rails, Active Storage, SQL.
-        </h3>
-      </div>
       <div class="spacer"></div>
       <Projects />
       <div class="spacer"></div>
